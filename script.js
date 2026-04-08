c82105584789af4eae0ec04be244307909af83b3
James Nzuki <nzukijames200@gmail.com>
2026-04-09 02:44:47 +0300

    config: update BACKEND_URL to Railway production

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const BACKEND_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:3000'
-    : 'https://omnidrive-backend.up.railway.app';
+    : 'https://omnidrive-backend-production.up.railway.app';
 
 
 
